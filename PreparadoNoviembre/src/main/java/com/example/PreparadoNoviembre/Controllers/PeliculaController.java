2fdb5ac04732f8685f4bb477c30f7d235fc99391
Adrian Rami Piramide <230053@fppiramide.com>
2025-11-26 09:52:32 +0100

    thymeleaf

--- PreparadoNoviembre/src/main/java/com/example/PreparadoNoviembre/Controllers/PeliculaController.java
+++ PreparadoNoviembre/src/main/java/com/example/PreparadoNoviembre/Controllers/PeliculaController.java
@@ -4,14 +4,16 @@ import com.example.PreparadoNoviembre.DAOs.DAOFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PeliculaController {
 
-    @GetMapping("/")
-    String main(Model model){
-        model.addAttribute("peliculas", DAOFactory.getInstance().getDaoPelicula().getListaPeliculas("Jorge"));
-        return "index";
+    @GetMapping("/pelisUser/{usuario}")
+    String main(Model model, @PathVariable String usuario){
+        System.out.println(usuario);
+        model.addAttribute("peliculas", DAOFactory.getInstance().getDaoPelicula().getListaPeliculas(usuario));
+        return "ver";
     }
 
 
--- PreparadoNoviembre/src/main/java/com/example/PreparadoNoviembre/Controllers/UsuarioController.java
+++ PreparadoNoviembre/src/main/java/com/example/PreparadoNoviembre/Controllers/UsuarioController.java
@@ -1,7 +1,15 @@
 package com.example.PreparadoNoviembre.Controllers;
 
+import com.example.PreparadoNoviembre.DAOs.DAOFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsuarioController {
+    @GetMapping("/")
+    String getUsers(Model model){
+        model.addAttribute("usuarios", DAOFactory.getInstance().getDaoUsuario().getUsers());
+        return "index";
+    }
 }
